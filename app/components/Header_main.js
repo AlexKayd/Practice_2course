4cc20d64bf3687f37d36f26874ed633e65991ea2
AlexKajdalova <AAKajdalova@mai.education>
2024-07-08 17:39:04 +0300

    Changes

--- app/components/Header_main.js
+++ app/components/Header_main.js
@@ -2,7 +2,7 @@ import './Header.css';
 
 const Header = ({text}) => {
     return (
-        <div className="header">
+        <div className="main_header">
           <h1 className="heading" data-text={text}>{text.substring(0, text.length / 2)}<br/>
           {text.substring(text.length / 2)}</h1>
         </div>
